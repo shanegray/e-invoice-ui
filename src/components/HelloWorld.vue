9f39a030f97774ca6cae3400df3605e48769535c
shanegray <shane.joseph.gray@gmail.com>
2020-09-02 10:38:34 +0100

    Added vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,19 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ msg }}</h1>
-    <h2>Shane Was HERE</h2>
-    <h3>Installed CLI Plugins</h3>   
-  </div>
+  </div>  
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+  import Vue from 'vue'
 
-export default Vue.extend({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-});
+  export default Vue.extend({
+    name: 'HelloWorld',
+    props: {
+      msg: {
+        type: String,
+        default: ""
+      },
+    },
+  })
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
