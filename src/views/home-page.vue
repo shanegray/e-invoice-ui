d8731e92b992c8dadbfe0811b23b46f13bff3772
shanegray <shane.joseph.gray@gmail.com>
2020-09-02 11:38:07 +0100

    Home page styling

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, eum recusandae accusamus cum eius sint commodi nostrum quidem quaerat perferendis, error sit qui, impedit necessitatibus laborum inventore quos laboriosam rerum.</h1>
+    <v-card class="mb-4">
+      <v-card-title>Upload XML File</v-card-title>
+      <v-card-text>
+        Upload Button
+      </v-card-text>
+    </v-card>
+    <v-card class="mb-4">
+      <v-card-title>Translate Invoice</v-card-title>
+      <v-card-text>
+        <v-container class="pa-0">
+          <v-row>
+            <v-col cols="12" sm="6" md="4">
+              <v-select label="From" :items="languages" />
+            </v-col>
+            <v-col cols="12" sm="6" md="4">
+              <v-select label="To" :items="languages"  />
+            </v-col>
+            <v-spacer></v-spacer>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary">Create Invoice</v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card>
+      <v-card-actions>
+        <v-btn @click="doSomething()" color="primary">Download XML</v-btn>
+        <v-btn color="primary">Download XSLT</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+  data() {
+    return {
+      languages: ["English", "Irish", "German"]
+    }
+  },
+  methods: {
+    doSomething() {
+      console.log("I work")
+    }
+  },
+}
 </script>
