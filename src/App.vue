0bfc429ad29692c14674f3d189a992231dbf6628
LeeGray1 <s00188181@mail.itsligo.ie>
2020-09-15 16:59:36 +0100

    Added back the title disappearing on small screens

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
     <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
-        <span>E-Invoice Translator</span>
+        <span class="hidden-sm-and-down">E-Invoice Translator</span>
       </v-toolbar-title>
         <v-spacer />
       <v-col cols="5" sm="3" md="3">
